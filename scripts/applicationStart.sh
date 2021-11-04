c48ab43f7b4c2e941e826d181f30d4dc922bb7cd
Pengcheng Xu <xu.pe@northeastern.edu>
2021-11-04 10:45:11 -0400

    almost finish the assign 5

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
 sudo nohup java -jar /home/ubuntu/app/demo-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
