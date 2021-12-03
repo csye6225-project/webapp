6102db20ce16bf34f47dbacaa3681b25196f98d9
Pengcheng Xu <xu.pe@northeastern.edu>
2021-12-03 11:32:47 -0500

    final edition of assign 7

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        System.setProperty("aws.sns.arn", "arn:aws:sns:us-east-1:370412597903:verification-notice");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
